813f3304fa89a8b992d0664bd627d4abec979dcb
Christopher Erb <christophererb01@gmail.com>
2023-07-31 16:35:55 -0400

    Chris

--- CED/src/components/contact.jsx
+++ CED/src/components/contact.jsx
@@ -7,13 +7,43 @@ const Contact = () => {
     
     
     return (
-        <div className="main-content">
+        <div className="front-page">
                   <header className="header">
         <h1>Contact Us!</h1>
       </header>
 
-      <h4>We always love to hear feedback! <br/> Here are some places you can find us.</h4>
-        <div className="iconFloat">
+
+      <h4 className=''>Looking for some drywall work?  Here's how you can find Scott!</h4>
+        <div className="iconFloat main-content">
+
+        <div className="notAnnoyed">
+          <h2>Facebook</h2>
+          <a href="https://www.facebook.com/scotterbdrywall">
+            <img src="FB.png" />
+          </a>
+        </div>
+
+        <div className="notAnnoyed">
+          <h2>Gmail</h2>
+          <a href="mailto:scotterbdrywall@gmail.com">
+            <img src="/gmail.png" />
+          </a>
+        </div>
+            <div className="notAnnoyed">
+          <h2>LinkedIn</h2>
+          <a href="https://www.linkedin.com/in/scott-erb-6397a110a/">
+            <img src="/icons8-linkedin-480.png" />
+          </a>
+        </div>
+
+        
+     </div>
+
+
+
+
+      <h4 className=''>I always love to hear website feedback!  Here you can find Christopher</h4>
+        <div className="iconFloat main-content">
         <div className="notAnnoyed">
           <h2>LinkedIn</h2>
           <a href="https://www.linkedin.com/in/christophererb1">
