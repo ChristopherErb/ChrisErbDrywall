edd280ce63345d39cda96ab5192ff1c4aba8a3b9
Christopher Erb <christophererb01@gmail.com>
2023-08-01 17:56:06 -0400

    Commits'

--- CED/src/components/customers.jsx
+++ CED/src/components/customers.jsx
@@ -4,18 +4,29 @@ import { useParams } from "react-router-dom";
 import NavBar from "./navbar";
 
 const Customers = (props) => {
+  const { customerId } = useParams();
+  const handleDelete = (customerId) => {
+    console.log(props.customers);
+    axios
+      .delete(`http://localhost:8000/customers/${customerId}`)
+      .then((response) => {
+        console.log("deleted", response.data);
+      })
+      .catch((error) => {
+        console.error("error deleting", error);
+      });
+  };
   return (
     <div>
       <header className="header">
         <h1>Our Loyal Customers</h1>
       </header>
-
       <div className="card-container">
         {props.customers.map((customer) => (
           <div className="card">
             <img className="card-img-top" src="jpg.jpg" alt="Card image cap" />
             <div className="card-body">
-              <h5 className="card-title" key={customer.customerId}>
+              <h5 className="card-title" key={customer.customer_Id}>
                 {customer.business_name}
               </h5>
               <p className="card-text">{customer.description}</p>
@@ -32,9 +43,9 @@ const Customers = (props) => {
               </li>
             </ul>
             <div className="card-body">
-              <a href="#" className="card-link">
-                Card link
-              </a>
+              <button onClick={() => handleDelete(customer.id)}>
+                Delete
+              </button>
               <a href="#" className="card-link">
                 Another link
               </a>
--- CED/src/components/estimates.jsx
+++ CED/src/components/estimates.jsx
@@ -1,25 +1,98 @@
-import axios from 'axios'
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import NavBar from './navbar'
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "./navbar";
 
 const Estimates = () => {
+  const initialState = {
+    business_name: "",
+    manager: "",
+    address: "",
+    price_sqft: "",
+    description: "",
+  };
 
+  const [formState, setFormState] = useState(initialState);
 
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.name]: event.target.value })
+  }
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    axios
+      .post('http://localhost:8000/customers/', formState)
+      .then((response) => {
+        console.log("cstmr created", response.data);
+        setFormState(initialState)
+      })
+      .catch((error) => {
+        console.error("error making cstmr", error)
+      })
+  }
 
-
-    return (
-
+  return (
+    <div>
+      <h2>Create New Customer</h2>
+      <form onSubmit={handleSubmit}>
         <div>
-        <header className="header">
-          <h1>Need an Estimate?</h1>
-        </header>
-
+          <label htmlFor="business_name">Business Name:</label>
+          <input
+            type="text"
+            id="business_name"
+            name="business_name"
+            value={formState.business_name}
+            onChange={handleChange}
+            required
+          />
         </div>
-    )
+        <div>
+          <label htmlFor="manager">Manager:</label>
+          <input
+            type="text"
+            id="manager"
+            name="manager"
+            value={formState.manager}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="address">Address:</label>
+          <input
+            type="text"
+            id="address"
+            name="address"
+            value={formState.address}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="price_sqft">Price per sqft:</label>
+          <input
+            type="text"
+            id="price_sqft"
+            name="price_sqft"
+            value={formState.price_sqft}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="description">Description:</label>
+          <textarea
+            id="description"
+            name="description"
+            value={formState.description}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Create Customer</button>
+      </form>
+    </div>
+  );
+};
 
-}
-
-
-export default Estimates
+export default Estimates;
